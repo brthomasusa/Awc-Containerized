5b7ccc3ab6a6606167d69c406fa617ba250866e2
bthomas <brthomasusa@gmail.com>
2024-11-27 20:52:47 -0600

    Git pwd test from rhel9-ws

--- src/Services/Company/Company.API/Program.cs
+++ src/Services/Company/Company.API/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 try
 {    
-    // Testing git
     builder.ConfigureSerilog();
     builder.Services.ConfigureOpenTelemetry();
     builder.Services.ConfigureHealthChecks();
